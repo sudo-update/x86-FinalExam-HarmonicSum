1a050bb0971b59f41235e614e3506694249fbf6d
sudo-update <seanjaviya@csu.fullerton.edu>
2021-05-20 12:30:11 -0700

    uploading delete tool

---
+++ deleteOldObjectFiles.sh
@@ -0,0 +1,5 @@
+rm Sum.lis Fill.lis Control.lis Harmonic_mean.lis
+echo "Removed Sum.lis Fill.lis Control.lis and others"
+rm SumArray.out FinalExam.out FinalProgram.out isfloat.o Main.o Sum.o Fill.o Display.o Control.o Harmonic_mean.o
+echo "Removed SumArray.out Main.o Sum.o Fill.o Display.o Control.o and others"
+echo "Terminating."
